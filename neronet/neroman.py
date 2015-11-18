d2c9f1e68fe8d541a7755dd1ee18770c91c01075
Donkeyhead <iiro.tahka@gmail.com>
2015-11-18 17:32:09 +0200

    Creates basic database load and experiment specification methods

--- neronet/neroman.py
+++ neronet/neroman.py
@@ -1,7 +1,43 @@
+import os.path
 import yaml
 
-class ConfigParser():
-    def __init__(self):
+CONFIG_FILENAME = 'config.yaml'
 
 class Neroman():
-    def __init__(self):
+    """
+    """
+    def __init__(self, database = "default.yaml"):
+        """
+        """
+        self.database = database
+        self.clusters = {}
+        self.experiments = {}
+        self.preferences = {}
+        self._load_database(database)
+
+    def _load_database(self, filename):
+        """Load the database from an yaml file"""
+        with open(filename, 'r') as file:
+            database = yaml.load(file.read())
+        self.clusters = database.get('clusters', {})
+        self.experiments = database.get('experiments', {})
+        self.preferences = database.get('preferences', {})
+    
+    def specify_experiment(self, folder):
+        """
+        """
+        with open(os.path.join(folder, CONFIG_FILENAME), 'r') as file:
+            experiment_data = yaml.load(file.read())
+        try:
+            experiment = {}
+            for field in ['run_command_prefix', 'main_code_file',
+                        'parameters', 'parameters_format']:
+                experiment[field] = experiment_data[field]
+            self.experiments[folder] = experiment
+        except KeyError:
+            print("Error while loading experiment")
+
+def main():
+    neroman = Neroman()
+if __name__ == '__main__':
+    main():
