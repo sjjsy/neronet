097bd2fd447e379df77fa041c4cef8314cd37169
Donkeyhead <iiro.tahka@gmail.com>
2015-11-27 18:09:17 +0200

    Adds more cli unittests

--- test/test_cli.py
+++ test/test_cli.py
@@ -25,7 +25,13 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(parsed.cluster[2], cluster_type)
 
     def test_specify_user(self):
-        folder_name = 'test_folder'
-        parsed = self.parser.parse_args(['--experiment', folder_name])
-        self.assertTrue(parsed.experiment)
-        self.assertEqual(parsed.experiment[0], folder_name)
+        name = 'Juha Pekka'
+        email = 'juha.pekka@supermail.orz'
+        parsed = self.parser.parse_args(['--user', name, email])
+        self.assertTrue(parsed.user)
+        self.assertEqual(parsed.user[0], name)
+        self.assertEqual(parsed.user[1], email)
+
+    def test_status(self):
+        parsed = self.parser.parse_args(['--status'])
+        self.assertTrue(parsed.status)
