d75f1a0fae30139dc54d5e06f225452acbbbd359
Donkeyhead <iiro.tahka@gmail.com>
2015-11-27 18:03:07 +0200

    Refactors some cli code and adds some cli test

---
+++ test/test_cli.py
@@ -0,0 +1,31 @@
+import unittest
+
+import neronet.cli as cli
+
+class TestCLI(unittest.TestCase):
+    
+    def setUp(self):
+        self.parser = cli.create_parser()
+
+    def test_specify_experiment(self):
+        folder_name = 'test_folder'
+        parsed = self.parser.parse_args(['--experiment', folder_name])
+        self.assertTrue(parsed.experiment)
+        self.assertEqual(parsed.experiment[0], folder_name)
+
+    def test_specify_cluster(self):
+        cluster_id = 'triton'
+        ssh_address = 'triton.aalto.fi'
+        cluster_type = 'slurm'
+        parsed = self.parser.parse_args(['--cluster', 
+                cluster_id, ssh_address, cluster_type])
+        self.assertTrue(parsed.cluster)
+        self.assertEqual(parsed.cluster[0], cluster_id)
+        self.assertEqual(parsed.cluster[1], ssh_address)
+        self.assertEqual(parsed.cluster[2], cluster_type)
+
+    def test_specify_user(self):
+        folder_name = 'test_folder'
+        parsed = self.parser.parse_args(['--experiment', folder_name])
+        self.assertTrue(parsed.experiment)
+        self.assertEqual(parsed.experiment[0], folder_name)
