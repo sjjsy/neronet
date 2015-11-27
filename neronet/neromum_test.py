87e46690df6c3c9851760d897388ce6848f4acdb
matte27 <matias.pitkanen1@gmail.com>
2015-11-27 17:20:53 +0200

    added unittests for neromum

--- neronet/neromum_test.py
+++ neronet/neromum_test.py
@@ -1,14 +1,32 @@
 import unittest
-import neromum
+import neronet.neromum
 import socket
 import os
+import pickle
 
-class Create_neromum(unittest.TestCase):
-    mum = Neromum()
-    mum.experiment = 'sleep.py'
-    mum.initialize_socket()
-    self.assertTrue(mum.sock)
-    self.assertTrue(isinstance( mum.port, int ))
-    self.assertTrue(mum.host == 'localhost')
-    self.assertTrue(mum.open_incoming_connections == [mum.sock])
-    self.assertTrue(mum.open_outgoing_connections == [])
+class Neromum_test(unittest.TestCase):
+
+    def setUp(self):
+        self.mum = Neromum()
+        self.mum.experiment = 'sleep.py'
+        self.d = { "lion": "yellow", "kitty": "red" }
+        self.data = pickle.dumps(self.d)
+        self.mum.initialize_socket()
+
+    def test_socket_init(self):
+
+        self.assertTrue(self.mum.sock)
+        self.assertTrue(isinstance( self.mum.port, int ))
+        self.assertEqual(self.mum.host,'localhost')
+        self.assertEqual(self.mum.open_incoming_connections, [mum.sock])
+        self.assertEqual(self.mum.open_outgoing_connections, [])
+
+    def test_data_from_nerokid(self):
+
+        self.mum.start_nerokid():
+
+    def test_parse_data(self):
+
+        self.mum.data = self.data
+        self.mum.parse_nerokid_data()
+        self.assertEqual(self.mum.data, self.d)
