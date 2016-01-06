1f8dbe6e4dbe725b53bbc04a28f1b71181857db9
blomqvt1 <teemu.blomqvist@aalto.fi>
2016-01-06 14:48:43 +0200

    Made a simple setup.py

---
+++ setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import sys
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+long_description = 'TODO'
+
+setup(
+    name='neronet',
+    version=1,
+    url='http://github.com/smarisa/neronet',
+    license='None',
+    author='Samuel Marisa & CO.',
+    install_requires=['Babel>=2.1.1',
+                      'pytz>=2015.7',
+                      'six>=1.10.0',
+                      'snowballstemmer>=1.2.0',
+                      'wheel>=0.26.0',
+                      'pyyaml>=3.11',
+                      ],
+    author_email='teemu.blomqvist@aalto.fi',
+    description='A tool for managing computational experiments.',
+    long_description=long_description,
+    #entry_points={},
+    packages=['neronet'],
+    include_package_data=True,
+    platforms='any',
+    zip_safe=False,
+    #package_data={'sandman': ['templates/**', 'static/*/*']},
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+    #extras_require={
+    #'testing': ['pytest'],
+    #}
+)
